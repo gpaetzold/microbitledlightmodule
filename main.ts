e7c3405bb092d2da56fcda1bec8e24361c26103f
gpaetzold <76201189+gpaetzold@users.noreply.github.com>
2021-09-29 11:34:02 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
 basic.showIcon(IconNames.Happy)
 basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P1, 0)
-    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    basic.pause(2000)
     pins.digitalWritePin(DigitalPin.P1, 1)
-    basic.pause(1000)
+    basic.pause(2000)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    basic.pause(2000)
 })
